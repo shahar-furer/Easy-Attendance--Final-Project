c75f21487152f33f9bf7bfe4dc3c0b818ed00d9a
tehilauzan <46137999+tehilauzan@users.noreply.github.com>
2020-12-02 00:08:23 +0200

    add file for connect to firebase

---
+++ app/src/main/java/com/example/easy_attendance/HoursTable.java
@@ -0,0 +1,14 @@
+package com.example.easy_attendance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HoursTable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hours_table);
+    }
+}

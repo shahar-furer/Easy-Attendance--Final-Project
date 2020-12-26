749ec619e6951f954b2601451c1fccb7467cc1af
Shahar <shachar.fo@gmail.com>
2020-12-27 00:18:30 +0200

    add menu ,timer and total to daily report

--- app/src/main/java/com/example/easy_attendance/DailyReport.java
+++ app/src/main/java/com/example/easy_attendance/DailyReport.java
@@ -7,38 +7,72 @@ import java.util.Date;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 
+import android.widget.Chronometer;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 import com.example.easy_attendance.firebase.model.FBAuth;
 import com.example.easy_attendance.firebase.model.FirebaseDBTable;
+import com.example.easy_attendance.firebase.model.FirebaseDBUser;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class DailyReport extends AppCompatActivity  implements View.OnClickListener {
 
     private Button start, end, logout;
+    private TextView helloUser;
+    public Chronometer chrom;
     FirebaseDBTable newAttendance = new FirebaseDBTable();
+    FirebaseDBUser userDB =new FirebaseDBUser();
     FBAuth auth = new FBAuth();
+    String uid= auth.getUserID();
+    String userName;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.daily_report);
+        userDB =new FirebaseDBUser();
         start = findViewById(R.id.startBtn);
         end = findViewById(R.id.endBtn);
         logout = findViewById(R.id.logoutBtn);
+        chrom = (Chronometer)findViewById(R.id.chronometerWatch);
+        helloUser= findViewById(R.id.textViewHello);
 
         start.setOnClickListener(this);
         end.setOnClickListener(this);
         logout.setOnClickListener(this);
+        chrom.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometerChanged) {
+                chrom = chronometerChanged;
+                long time = SystemClock.elapsedRealtime() - chrom.getBase();
+                int h   = (int)(time /3600000);
+                int m = (int)(time - h*3600000)/60000;
+                int s= (int)(time - h*3600000- m*60000)/1000 ;
+                String t = (h < 10 ? "0"+h: h)+":"+(m < 10 ? "0"+m: m)+":"+ (s < 10 ? "0"+s: s);
+                chrom.setText(t);
+            }
+        });
+        findUserName();
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -65,14 +99,22 @@ public class DailyReport extends AppCompatActivity  implements View.OnClickListe
         return super.onOptionsItemSelected(item);
     }
 
+
+
     @Override
     public void onClick(View v) {
         if (v == start) {
-            newAttendance.addEntryToDB(new Date(), "entry");
+            newAttendance.addEntryToDB(new Date(), chrom);
         }
 
         if (v == end) {
-            newAttendance.addExitToAttendance(new Date());
+            LinearLayout ll =findViewById(R.id.totalHours);
+            newAttendance.addExitToAttendance(new Date() , ll);
+
+
+//            Snackbar.make(findViewById(R.id.totalHours), "Your total hours is" +total,
+//                    Snackbar.LENGTH_SHORT)
+//                    .show();
         }
 
         if (v == logout) {
@@ -85,4 +127,24 @@ public class DailyReport extends AppCompatActivity  implements View.OnClickListe
         }
 
     }
+
+    private void findUserName() {
+
+       DatabaseReference userRef =userDB.getUserFromDB(uid);
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+               userName =dataSnapshot.child("fName").getValue(String.class);
+                helloUser.append("Hello "+userName);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+
+    }
+
+
 }
--- app/src/main/java/com/example/easy_attendance/firebase/model/FirebaseDBTable.java
+++ app/src/main/java/com/example/easy_attendance/firebase/model/FirebaseDBTable.java
@@ -1,9 +1,14 @@
 package com.example.easy_attendance.firebase.model;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
 import com.example.easy_attendance.firebase.model.FirebaseBaseModel;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -11,6 +16,8 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import android.widget.Chronometer;
+import android.os.SystemClock;
 import java.util.HashMap;
 
 
@@ -20,12 +27,14 @@ public class FirebaseDBTable extends FirebaseBaseModel {
     SimpleDateFormat dayFormat = new SimpleDateFormat("dd");
     SimpleDateFormat hourFormat = new SimpleDateFormat("HH:mm:ss");
     SimpleDateFormat hourOnlyFormat = new SimpleDateFormat("HH");
+    private String totalHours = "0";
 
     FBAuth mAuth = new FBAuth();
     String uid = mAuth.getUserID();
     FirebaseDBUser userDB = new FirebaseDBUser();
     DatabaseReference userRef = userDB.getUserFromDB(uid);
     String keyId;
+    Chronometer chronomoter;
 
     public FirebaseDBTable()
     {
@@ -41,17 +50,19 @@ public class FirebaseDBTable extends FirebaseBaseModel {
         });
     }
 
-    public void addEntryToDB(Date d, String s)
+    public void addEntryToDB(Date d, Chronometer c)
     {
 
         String year = yearFormat.format(d);
         String month = monthFormat.format(d);
         String day = dayFormat.format(d);
         String hour= hourFormat.format(d);
+        chronomoter=c;
         myRef.child("Attendance").child(keyId).child(year).child(month).addListenerForSingleValueEvent(new ValueEventListener() {
         @Override
         public void onDataChange(DataSnapshot snapshot) {
             if (!(snapshot.hasChild(day))) {
+            startChrom();
                 writeNewAttendance( year,month, day ,hour);
             }
         }
@@ -74,7 +85,7 @@ public class FirebaseDBTable extends FirebaseBaseModel {
         myRef.child("Attendance").child(keyId).child(year).child(month).child(day).child("entry").setValue(hour); //if we build the DB as hashMap, then will change the Entry/Exit
     }
 
-    public void addExitToAttendance(Date d)
+    public void addExitToAttendance(Date d, LinearLayout ll)
     {
         String year = yearFormat.format(d);
         String month = monthFormat.format(d);
@@ -86,6 +97,14 @@ public class FirebaseDBTable extends FirebaseBaseModel {
                 if (snapshot.hasChild(day)) {
                     if ((snapshot.child(day).hasChild("entry"))&&(!(snapshot.child(day).hasChild("exit")))) {
                         myRef.child("Attendance").child(keyId).child(year).child(month).child(day).child("exit").setValue(hour); //if we build the DB as hashMap, then will change the Entry/Exit
+                        stopChrom();
+                        String total= (String) chronomoter.getText();
+                        myRef.child("Attendance").child(keyId).child(year).child(month).child(day).child("total").setValue(total);
+                        totalHours = total;
+                        Snackbar.make(ll, "Your total hours is" +total,
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+
                     }
                 }
                 else {
@@ -97,11 +116,27 @@ public class FirebaseDBTable extends FirebaseBaseModel {
                             if ((snapshot.hasChild("entry"))&&Integer.parseInt(hourOnlyFormat.format(d))<10) {
                                 if (!(snapshot.hasChild("exit"))) {
                                     myRef.child("Attendance").child(keyId).child(year).child(month).child(Integer.toString(prevDay)).child("exit").setValue(hour); //if we build the DB as hashMap, then will change the Entry/Exit
+                                    String entryHour =snapshot.child("entry").getValue(String.class);
+                                    String exitHour =hour;
+                                    int t1 = getTotalMinutes(entryHour);
+                                    int t2 = getTotalMinutes(exitHour);
+                                    int result = Math.abs(t1 - t2);
+                                    String total = getResult(result);
+                                    myRef.child("Attendance").child(keyId).child(year).child(month).child(Integer.toString(prevDay)).child("total").setValue(total); //if we build the DB as hashMap, then will change the Entry/Exit
+                                    Snackbar.make(ll, "Your total hours is" +total,
+                                    Snackbar.LENGTH_SHORT)
+                                    .show();
+
                                 }
                             }
                             else {
                                 myRef.child("Attendance").child(keyId).child(year).child(month).child(day).child("entry").setValue("09:00:00"); //if we build the DB as hashMap, then will change the Entry/Exit
-                                myRef.child("Attendance").child(keyId).child(year).child(month).child(day).child("exit").setValue(hour); //if we build the DB as hashMap, then will change the Entry/Exit
+                                myRef.child("Attendance").child(keyId).child(year).child(month).child(day).child("exit").setValue(hour);
+                                myRef.child("Attendance").child(keyId).child(year).child(month).child(day).child("total").setValue("0");//if we build the DB as hashMap, then will change the Entry/Exit
+                                Snackbar.make(ll, "Your total hours is 0 ,no entry was registered" ,
+                                Snackbar.LENGTH_SHORT)
+                                .show();
+
                             }
                         }
 
@@ -124,5 +159,30 @@ public class FirebaseDBTable extends FirebaseBaseModel {
    public DatabaseReference getAttendanceFromDB (String month) //not finished. need loop for printing all the dates
    {
        return myRef.getRef().child("Attendance").child(uid).child(month);
+   }
+
+
+    public void startChrom(){
+
+        chronomoter.setBase(SystemClock.elapsedRealtime());
+        chronomoter.setText("00:00:00");
+        chronomoter.start();
+
+    }
+
+    public void stopChrom(){
+        chronomoter.stop();
+    }
+
+
+    public static int getTotalMinutes(String time) {
+        String[] t = time.split(":");
+        return Integer.valueOf(t[0]) * 60 + Integer.valueOf(t[1]);
+    }
+
+    public static String getResult(int total) {
+        int minutes = total % 60;
+        int hours = ((total - minutes) / 60) % 24;
+        return String.format("%02d:%02d", hours, minutes);
     }
 }
